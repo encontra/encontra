fd8d9bbba717e1be2c0d821433e8d1809b15d68c
Nelson Silva <nelson.silva@inevo.pt>
2013-07-04 15:39:40 +0100

    Clone repos with HTTPS

--- clone.sh
+++ clone.sh
@@ -5,5 +5,5 @@ modules=`grep -oP "(?<=\<module>).*(?=\</module>)" pom.xml`
 for module in $modules
 do
 echo "Cloning ${underline}$module${normal}"
-git clone git@github.com:encontra/$module.git
+git clone https://github.com/encontra/$module.git
 done
